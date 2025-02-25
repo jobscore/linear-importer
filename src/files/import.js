4b67df63e154aa51bc7006681e71daba1ce68ec1
Glauber Batista <glauber@jobscore.com>
2025-02-25 14:43:45 -0300

    fix: do not check if file or blob
    the check is breaking the file upload. Also, the only call to that method is passing an object like file, not a File or Blob

--- src/files/import.js
+++ src/files/import.js
@@ -7,39 +7,34 @@ async function uploadFileToLinear(file, issueId) {
   if (ENABLE_DETAILED_LOGGING) {
     console.log(chalk.magenta("Input file object:"), file);
     console.log(chalk.magenta("Type of file:"), typeof file);
-    console.log(chalk.magenta("Is File?"), file instanceof File);
-    console.log(chalk.magenta("Is Blob?"), file instanceof Blob);
   }
 
   let fileName = file.name;
   let fileType = file.type;
   let fileSize = file.size;
 
-  if (!(file instanceof File) && !(file instanceof Blob)) {
-    if (
-      typeof file === "object" &&
-      file !== null &&
-      "name" in file &&
-      "type" in file &&
-      "size" in file
-    ) {
-      try {
-        if (file.arrayBuffer) {
-          const buffer = await file.arrayBuffer();
-          file = new Blob([buffer], { type: file.type });
-        } else {
-          file = new Blob([file], { type: file.type });
-        }
-        // console.log(chalk.yellow("Created Blob from file object"));
-      } catch (e) {
-        console.error("Failed to create Blob from file object:", e);
-        process.exit(0);
+  if (
+    typeof file === "object" &&
+    file !== null &&
+    "name" in file &&
+    "type" in file &&
+    "size" in file
+  ) {
+    try {
+      if (file.arrayBuffer) {
+        const buffer = await file.arrayBuffer();
+        file = new Blob([buffer], { type: file.type });
+      } else {
+        file = new Blob([file], { type: file.type });
       }
-    } else {
-      throw new Error(
-        "Invalid file object. Expected File or Blob, or an object with name, type, and size properties.",
-      );
+    } catch (e) {
+      console.error("Failed to create Blob from file object:", e);
+      process.exit(0);
     }
+  } else {
+    throw new Error(
+      "Invalid file object. Expected File or Blob, or an object with name, type, and size properties.",
+    );
   }
 
   if (ENABLE_DETAILED_LOGGING) {
